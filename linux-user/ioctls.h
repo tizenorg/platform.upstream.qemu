vliaskov/qemu

linux-user: implement FS_IOC_SETFLAGS ioctl
Signed-off-by: Alexander Graf <agraf@suse.de>

linux-user/ioctls.h
@@ -87,6 +87,7 @@
                    MK_PTR(MK_STRUCT(STRUCT_fiemap)))
 #endif
   IOCTL(FS_IOC_GETFLAGS, IOC_R, MK_PTR(TYPE_INT))
+  IOCTL(FS_IOC_SETFLAGS, IOC_W, MK_PTR(TYPE_INT))
 
   IOCTL(SIOCATMARK, 0, TYPE_NULL)
   IOCTL(SIOCADDRT, IOC_W, MK_PTR(MK_STRUCT(STRUCT_rtentry)))

tizenorg/platform.upstream.qemu

linux-user/ioctls.h
@@ -91,6 +91,7 @@
                    MK_PTR(MK_STRUCT(STRUCT_fiemap)))
 #endif
   IOCTL(FS_IOC_GETFLAGS, IOC_R, MK_PTR(TYPE_LONG))
+  IOCTL(FS_IOC_SETFLAGS, IOC_W, MK_PTR(TYPE_LONG))
 
   IOCTL(SIOCATMARK, 0, TYPE_NULL)
   IOCTL(SIOCGIFNAME, IOC_RW, MK_PTR(TYPE_INT))